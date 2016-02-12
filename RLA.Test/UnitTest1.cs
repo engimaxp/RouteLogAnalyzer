f152ddb007af5f1edc1e2185a7e01de4da4b90cd
enigmaxp <wxf_072445@outlook.com>
2016-02-12 09:38:59 +0800

    引入单元测试

---
+++ RLA.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RLA.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            string haha = "haha";
+            Assert.AreEqual(haha, "haha");
+        }
+    }
+}
